4611cf0774c7589a522424504dfc537a1aef4cf7
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-03-04 16:59:39 +0700

    Create Account.go

---
+++ Console/Account.go
@@ -0,0 +1,8 @@
+package main
+import ("fmt")
+
+func main() {
+  var AccountID string = "f160bb4a1a2943cc882898a2b80463bd" //type is string
+  var Enterprise = "Sachtony" //type is inferred
+  fmt.Println(Enterprise)
+}
